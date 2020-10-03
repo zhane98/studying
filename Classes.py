3e59baab2e716227a72e1151aa12de74366b86ca
zhane98 <71723981+zhane98@users.noreply.github.com>
2020-10-03 10:45:45 +0100

    Codewars1 and Classes

---
+++ Classes.py
@@ -0,0 +1,54 @@
+# class Person:
+#     def __init__(self, name):
+#         self.name = name 
+
+# person = Person("Dave Smith")
+# print(person.name)
+
+# PARENT / blueprint
+class Animal: #0 initiate an object/class to be reused
+    def __init__(self, colour, first_name, last_name, age_int): #2 arguments get passed in the same order
+        self.colour = colour #3 we assign arguments to parts of the object/class body
+        self.first = first_name
+        self.last = last_name
+        self.age = age_int              #These are the different compartments in the empty box
+
+    def rollover(self): # Adding a function/action called rollover to the blueprint for the clones to inherit.
+        print(self.first + " rolls over")
+
+
+# CHILD / clone
+new_animal1 = Animal("Pink", "Tia", "Woods", 20) #1 pass in correct number of arguments.     We start adding things in order into the box/giving meaning.
+
+# CHILD / clone
+new_animal2 = Animal("Black", "Casius", "Clay", 120)
+
+# print(new_animal1.first)
+# print(new_animal2.first)
+
+new_animal1.rollover() # The clones have now inherited the ability to rollover from their PARENT blueprint
+new_animal2.rollover()
+
+# This is how Inheritance works
+# Chimp is a new Parent class based off of the Animal Parent Class.
+# All initiations have to be copied but the functions are implicitly inherited 
+class Chimp(Animal):
+    def __init__(self, colour, first_name, last_name, age_int, breed):
+        super().__init__(colour, first_name, last_name, age_int)
+        self.colour = colour 
+        self.first = first_name
+        self.last = last_name
+        self.age = age_int 
+        self.breed = breed
+
+    def smash_rocks(self):      #defining a new function
+        print(self.first + ", go and smash the" + colour_of_rocks_to_smash + "rocks")
+
+# CHILD of Chimp PARENT class.
+leader = Chimp("Black", "Caesar", "Emperor", 40, "Silverback")
+
+print(leader.first)
+print(leader.breed)
+
+leader.rollover()
+leader.smash_rocks()
---
+++ codewars1.py
@@ -0,0 +1,39 @@
+def is_divide_by(number, a, b):
+  if (number % a == 0 and number % b == 0):
+    return(True)
+  else:
+    return(False) # not converting True or False to strings, we're just returning the Boolean, and can only return 1 thing.
+    
+is_divide_by(-12, 2, -6)
+is_divide_by(-12, 2, -5)
+
+# array/list
+
+#for i in range(3, 12):
+    #print(i)
+
+def arr(n = 0): # function with default value
+    new_list = [] # Empty list to append items to later
+
+    for num in range(n): 
+        # range with 1 number will go from 0 -> n automatically
+        # loop over the range and for each **num** in range 0 -> n, append it to the empty new_list
+        new_list.append(num)
+        
+    return(new_list) 
+
+print(arr())
+
+
+# for n in arr(n):
+#     print([0,  n - 1])
+
+
+
+    # [ the numbers 0 to N-1 ] range, default value. Range into a list, and then into a return, as only 1 can be returned.
+    # new_array = [] to create an empty list 
+
+
+
+# any value of n, want to just return a list of 0 to n-1
+# need a loop, to read that it's given 0, then continue until n-1 is reached 
