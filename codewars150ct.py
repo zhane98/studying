5b1f2006a2596569fe595b988be004e0c9483769
zhane98 <71723981+zhane98@users.noreply.github.com>
2020-10-15 19:57:45 +0100

    Code Wars 15th October

---
+++ codewars150ct.py
@@ -0,0 +1,21 @@
+# def double_integer(i):
+#     return i * 2
+
+# print(double_integer(2))
+
+
+# tidy up parameter names
+# if statement to check first argument
+# if 1st argument is R, then sort into ascending order
+# else, sort and reverse order.
+
+def flip(a, d):
+
+
+
+
+# print(flip('R', [3, 2, 1, 2]))
+# print(flip('L', [3, 2, 1, 2]))
+
+# flip('R', [3, 2, 1, 2])     =>  [1, 2, 2, 3]
+# flip('L', [1, 4, 5, 3, 5])  =>  [5, 5, 4, 3, 1]
--- practice_codewars.py
+++ practice_codewars.py
@@ -1,6 +1,12 @@
-# def arr(n = 0):
-
-# for n in range(n): 
-#     arr.append[
+# define a function that takes a number
+# print a range of numbers from 0 to n-1
+# create a list
+# get range inside the list
+# return list
 
 def arr(n = 0):
+
+    return list(range(n)) 
+
+print(arr(10))
+
