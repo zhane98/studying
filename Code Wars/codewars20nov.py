53a5b893d159f295a6f423a9d5d8894f2a4d5259
zhane98 <71723981+zhane98@users.noreply.github.com>
2020-11-20 18:59:58 +0000

    codewars20nov.py

---
+++ Code Wars/codewars20nov.py
@@ -0,0 +1,46 @@
+#https://www.codewars.com/kata/5a4ea304b3bfa89a9900008e/train/python
+
+# def max_number(n):
+
+#     lis = list(str(n))
+    
+#     sort = sorted(lis, reverse=True)
+    
+#     last = ''.join(sort)
+
+#     return int(last)
+
+# print(max_number(213)) #, 321)
+# print(max_number(7389)) #, 9873)
+# print(max_number(63792)) #, 97632)
+# print(max_number(1000000)) #, 1000000)
+
+
+
+
+#https://www.codewars.com/kata/565ce4ab24ef4aee6a000074/train/python
+
+def is_matching(st, st1, st2): 
+  
+#     strip = st.replace(" ", "")
+#     strip1 = st1.replace(" ", "")
+#     strip2 = st2.replace(" ", "")
+
+
+#     lis = list(strip.lower())
+#     lis1 = list(strip1.lower())
+#     lis2 = list(strip2.lower())
+    
+#     check = lis1 + lis2
+
+#     sort = sorted(lis)
+#     sort12 = sorted(check)
+    
+#     return sort == sort12
+    
+# print(is_matching("bouh", "0", "buh")) #, False)
+# print(is_matching("kitten", "t", "eink")) #, False)
+# print(is_matching("mentalist", "l.st", "metan")) #, False)
+# print(is_matching("impressionistic", "isis isi", "precomnt"))#, True)
+# print(is_matching("email box", "bail", "moxe")) #, True)
+# print(is_matching("detail", "tlei", "dai")) #, False)    
