5f917c68ffb1ae94f2994003403dcf445f8e730d
zhane98 <71723981+zhane98@users.noreply.github.com>
2021-01-04 20:06:26 +0000

    codewars04jan.py

---
+++ Code Wars/codewars04jan.py
@@ -0,0 +1,68 @@
+import math
+
+#https://www.codewars.com/kata/5a023c426975981341000014/train/python
+
+# def other_angle(a, b):
+
+#     return 180 - (a + b)
+
+# print(other_angle(30, 60)) #, 90)
+# print(other_angle(60, 60)) #, 60)
+# print(other_angle(43, 78)) #, 59)
+# print(other_angle(10, 20)) #, 150)
+
+
+
+
+# #https://www.codewars.com/kata/5748838ce2fab90b86001b1a
+
+# def square_area(A):
+
+#     #math.sqrt
+#     #Area = 0.5 r2 theta
+#     # r = A / math.sqrt(0.5 x )
+   
+
+
+# print(square_area(2)) #, 1.62)
+# print(square_area(0)) #, 0)
+# print(square_area(14.05)) #, 80)
+# print(square_area(1)) #, 0.41)
+# print(square_area(100)) # , 4052.85)
+
+# #area of red square 
+# #arc = 0.5 r2 theta
+
+
+
+#https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+
+# def no_space(x):
+#     remove = x.replace(" ", "")
+    
+    
+#     return remove
+
+
+
+
+# print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B')) #, '8j8mBliB8gimjB8B8jlB')
+# print(no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')) #, '88Bifk8hB8BB8BBBB888chl8BhBfd')
+# print(no_space('8aaaaa dddd r     ')) #, '8aaaaaddddr')
+# print(no_space('jfBm  gk lf8hg  88lbe8 ')) #, 'jfBmgklf8hg88lbe8') 
+
+
+
+#https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+
+def monkey_count(n):
+
+    test = list(range(1, n + 1))
+   
+    
+    return test
+
+print(monkey_count(5)) #, [1, 2, 3, 4, 5])
+print(monkey_count(3)) #, [1, 2, 3])
+print(monkey_count(9)) #, [1, 2, 3, 4, 5, 6, 7, 8, 9])
